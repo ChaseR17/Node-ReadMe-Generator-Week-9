b3df128a95fa7d3b68b2e8dcae2fd6387e57935a
Chase Gilhart <chasegilhart@Chases-MacBook-Pro.local>
2024-06-03 09:25:45 -0400

    got it to run the questions

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -6,13 +6,86 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+// Add in the badges for the different licenses
+function validateContent(value) {
+    if (value != "") {
+        return true;
+    }
+    else {
+        return "Please fill in question";
+    }
+}
+
+const questions = [
+
+    // title
+    {
+        type: "input",
+        name: "title",
+        message: "What is your projects title?",
+        validate: validateContent,
+    },
+    // description
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project",
+        validate: validateContent,
+    },
+    // installation
+    {
+        type: "input",
+        name: "installation",
+        message: "Directions on how to install?",
+        validate: validateContent,
+    },
+    //usage
+    {
+        type: "input",
+        name: "usage",
+        message: "How to use?",
+        validate: validateContent,
+    },
+    // pick a license
+
+    // how to contribute
+    {
+        type: "input",
+        name: "contribution",
+        message: "How can others add to your project?",
+        validate: validateContent,
+    },
+    // tests
+    {
+        type: "input",
+        name: "tests",
+        message: "Testing Instructions?",
+        validate: validateContent,
+    },
+    // github name
+    {
+        type: "input",
+        name: "GitHub",
+        message: "What is your GitHub username?",
+        validate: validateContent,
+    },
+    // email
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email",
+        validate: validateContent,
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
